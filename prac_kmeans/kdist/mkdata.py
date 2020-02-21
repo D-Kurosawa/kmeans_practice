4a2dad396237b3bc1eee4b3006a09b61d1d51bfa
Daisuke Kurosawa <dai-kuro@sings.jp>
2020-02-21 11:31:08 +0900

    [add] bi plot

--- prac_kmeans/kdist/mkdata.py
+++ prac_kmeans/kdist/mkdata.py
@@ -45,10 +45,11 @@ def standards(df):
 
     contribution_rate(pca)
     factor_loading(pca, df)
+    show_biplot(pca, df_pca, df)
 
 
 def contribution_rate(pca):
-    """寄与率を確認"""
+    """寄与率"""
     cumulative_contribution_ratio = np.cumsum(pca.explained_variance_ratio_)
     plt.figure(figsize=(10, 5))
     plt.plot(list(range(1, len(cumulative_contribution_ratio) + 1)),
@@ -71,6 +72,30 @@ def factor_loading(pca, df):
     print(pd.DataFrame(
         data={'factor_loading': pca.components_[1]},
         index=df.columns).sort_values(by='factor_loading', ascending=False))
+
+
+def show_biplot(pca, df_pca, df):
+    # グラフを表示する
+    plt.figure(figsize=(10, 10))
+
+    # 因子スコアのプロット
+    plt.scatter(df_pca.iloc[:, 0], df_pca.iloc[:, 1])
+
+    # 因子負荷量のプロット
+    # 第一主成分と第2主成分の因子負荷量を取得
+    pc0 = pca.components_[0]
+    pc1 = pca.components_[1]
+    arrow_mul = 4
+    text_mul = 1.2
+    for i in range(len(pc0)):
+        plt.arrow(0, 0, pc0[i] * arrow_mul, pc1[i] * arrow_mul, color='r')
+        plt.text(pc0[i] * arrow_mul * text_mul, pc1[i] * arrow_mul * text_mul,
+                 df.columns[i], color='r', fontsize=14)
+    plt.title('biplot')
+    plt.xlabel('pc1')
+    plt.ylabel('pc2')
+    plt.grid(True)
+    plt.show()
 
 
 if __name__ == '__main__':
